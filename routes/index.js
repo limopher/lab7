07faf49cb57ce90b8a4e8fc700429e27b9f77cf0
Chris Lim <cklim@ucsd.edu>
2016-02-18 11:07:24 +0000

    gridExperiment

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,13 @@ exports.viewGrid = function(req, res){
 };
 
 exports.view = function(req, res){
-  	projects["grid"] = false;
+	var random_num = Math.random();
+	console.log(random_num);
+
+	if (random_num >= 0) {
+ 	 projects['grid'] = false;
   	res.render('index', projects);
+	} else {
+  	res.redirect('/grid');
+	}  	
 };
